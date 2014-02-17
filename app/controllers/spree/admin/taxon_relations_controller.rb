439ecedb299aabe65de836c16b651c00f34ff793
GarPit <garik.piton@gmail.com>
2014-02-17 12:26:59 +0400

    paginate taxon relations

--- app/controllers/spree/admin/taxon_relations_controller.rb
+++ app/controllers/spree/admin/taxon_relations_controller.rb
@@ -1,6 +1,10 @@
 module Spree
   module Admin
     class TaxonRelationsController < ResourceController
+      def index
+        @taxon_relations = @taxon_relations.order("created_at desc").page(params[:page]).per(Spree::Config[:admin_products_per_page])
+      end
+
       def attach
         Spree::AttachProductsWorker.perform_async(params[:id])
         redirect_to admin_taxon_relations_path, :notice => Spree.t(:attaching_products_started)
